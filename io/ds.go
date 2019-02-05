3dfeba4a39f315b44e17b51be3f2a75e1e35967c
Just1689 <Just1689@gmail.com>
2019-02-05 20:51:50 +0200

    Refactor

--- io/ds.go
+++ io/ds.go
@@ -20,20 +20,17 @@ func GetDataStoreKey(collection, id string) *datastore.Key {
 
 func DefaultConfigFunc(projectId string, config *Configuration) {
 
-	stackdriverExporter, err := stackdriver.NewExporter(stackdriver.Options{ProjectID: projectId})
+	stackDriverExporter, err := stackdriver.NewExporter(stackdriver.Options{ProjectID: projectId})
 	if err != nil {
 		config.Err = err
 		return
 	}
-
-	trace.RegisterExporter(stackdriverExporter)
+	trace.RegisterExporter(stackDriverExporter)
 	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
-
 	client, err := datastore.NewClient(context.Background(), projectId)
 	if err != nil {
 		config.Err = err
 		return
 	}
-
 	config.DataStoreClient = client
 }

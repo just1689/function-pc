e773b68ee78e5912b1a9e305987a81b0fb50e165
Just1689 <Just1689@gmail.com>
2019-02-05 20:14:27 +0200

    Route get and set

--- function.go
+++ function.go
@@ -1,24 +1,14 @@
 package F
 
 import (
-	"cloud.google.com/go/datastore"
-	"context"
-	"contrib.go.opencensus.io/exporter/stackdriver"
-	"crypto/sha1"
-	"encoding/base64"
-	"encoding/json"
-	"fmt"
 	"github.com/plancks-cloud/function-pc/io"
-	"go.opencensus.io/trace"
-	"io/ioutil"
 	"net/http"
 	"os"
 )
 
 var (
 	config     *io.Configuration
-	configFunc = defaultConfigFunc
-	pwd        = os.Getenv("pwd")
+	configFunc = io.DefaultConfigFunc
 	projectId  = os.Getenv("GCP_PROJECT")
 )
 
@@ -27,114 +17,63 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 	action := r.URL.Query().Get("action")
 	collection := r.URL.Query().Get("collection")
 
-	if githubRepo == "" {
-		io.WriteError(w, http.StatusBadRequest, "Bad request: ?project=X found.")
+	if action == "" || collection == "" {
+		io.WriteError(w, http.StatusBadRequest, "Bad request: action and collection required.")
 		return
 	}
 
 	config = &io.Configuration{}
-	config.Once.Do(func() { configFunc() })
+	config.Once.Do(func() { configFunc(projectId, config) })
 
-	//Get clones from Github API
-	u, err := getClones(githubRepo)
-	if err != nil {
-		fmt.Println(err)
-		io.WriteError(w, http.StatusBadRequest, err.Error())
+	id := r.Header.Get("persist-id")
+	key := r.Header.Get("persist-key")
+
+	authenticated := io.Auth(id, key)
+	if !authenticated {
+		io.WriteError(w, http.StatusUnauthorized, "Unauthorized to access. Check ID and key.")
 		return
 	}
 
-	//Store all the clone data
-	storeObjects(githubRepo, u)
-
-	days, err := config.ListAll(githubRepo)
-	if err != nil {
-		fmt.Println(err)
-		io.WriteError(w, http.StatusInternalServerError, err.Error())
+	if action == "get" {
+		handleGet(collection, w)
+	} else if action == "set" {
+		handleSet()
+	} else {
+		io.WriteError(w, http.StatusBadRequest, "Bad request: action must be get or set.")
 		return
 	}
 
-	//Total unique clones
-	total := countUniques(days)
-
-	//Create an url for the shield
-	url := fmt.Sprintf("https://img.shields.io/badge/Unique%sclones-%v-brightgreen.svg", " ", total)
-
-	//Hash the url
-	sha := shaStr(url)
-
-	w.Header().Add("Content-type", "image/svg+xml;charset=utf-8")
-	w.Header().Add("Cache-Control", "no-cache")
-	w.Header().Add("ETag", sha)
-	io.WriteURLToWriter(url, w)
 }
 
-func getClones(project string) (clones *io.Clones, err error) {
-	fmt.Println("Looking up clones on Github for project: ", project)
-	url := fmt.Sprintf("https://api.github.com/repos/%v/traffic/clones", project)
-	req, _ := http.NewRequest("GET", url, nil)
-
-	req.Header.Add("Authorization", pwd)
-	req.Header.Add("cache-control", "no-cache")
-	req.Header.Add("Postman-Token", "102f0b23-c735-4d04-bb80-c1e099502d37")
-
-	res, err := http.DefaultClient.Do(req)
-	body, _ := ioutil.ReadAll(res.Body)
-	defer res.Body.Close()
-	if err != nil {
-		fmt.Println(err)
+func handleGet(collection string, w http.ResponseWriter) {
+	if collection == io.RouteCollectionName {
+		sl, err := io.ListAllRoutes(config)
+		if err != nil {
+			io.WriteError(w, http.StatusInternalServerError, "Could not list all routes")
+			return
+		}
+		io.WriteObjectToJson(w, sl)
+	} else if collection == io.ServiceCollectionName {
+		sl, err := io.ListAllServices(config)
+		if err != nil {
+			io.WriteError(w, http.StatusInternalServerError, "Could not list all services")
+			return
+		}
+		io.WriteObjectToJson(w, sl)
+	} else {
+		io.WriteError(w, http.StatusBadRequest, "Bad request: action must be get or set.")
 		return
 	}
 
-	clones = &io.Clones{}
-	err = json.Unmarshal(body, clones)
-	return
-
 }
 
-func getDataStoreKeys(project string, clones *io.Clones) (keys []*datastore.Key) {
-	keys = make([]*datastore.Key, len(clones.Days))
-	for _, c := range clones.Days {
-		key := datastore.NameKey(project, c.Timestamp, nil)
-		keys = append(keys, key)
+func handleSet(collection string, config io.Configuration, id string) {
+	if collection == io.RouteCollectionName {
+
+		io.StoreRoutes(config, id)
 	}
-	return keys
-}
-
-func countUniques(days []*io.Day) int {
-	total := 0
-	for _, d := range days {
-		total += d.Uniques
-	}
-	return total
-}
-
-func shaStr(str string) string {
-	hasher := sha1.New()
-	hasher.Write([]byte(str))
-	sha := base64.URLEncoding.EncodeToString(hasher.Sum(nil))
-	return sha
 }
 
 func init() {
 	config = &io.Configuration{}
-}
-
-func defaultConfigFunc() {
-
-	stackdriverExporter, err := stackdriver.NewExporter(stackdriver.Options{ProjectID: projectId})
-	if err != nil {
-		config.Err = err
-		return
-	}
-
-	trace.RegisterExporter(stackdriverExporter)
-	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
-
-	client, err := datastore.NewClient(context.Background(), projectId)
-	if err != nil {
-		config.Err = err
-		return
-	}
-
-	config.DataStoreClient = client
 }

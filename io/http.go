03c382a8b57b37163f600fd643f0c92428893c8d
Just1689 <Just1689@gmail.com>
2019-02-12 22:14:46 +0200

    bug fix

--- io/http.go
+++ io/http.go
@@ -2,29 +2,20 @@ package io
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/sirupsen/logrus"
-	"io/ioutil"
 	"net/http"
 )
-
-func WriteURLToWriter(url string, w http.ResponseWriter) {
-	req, _ := http.NewRequest("GET", url, nil)
-	res, _ := http.DefaultClient.Do(req)
-	body, _ := ioutil.ReadAll(res.Body)
-	defer res.Body.Close()
-	_, err := w.Write(body)
-	if err != nil {
-		fmt.Println(err)
-	}
-}
 
 func WriteError(w http.ResponseWriter, code int, msg string) {
 	w.WriteHeader(code)
 	w.Header().Add("Content-type", "text")
 	w.Header().Add("Cache-Control", "no-cache")
 	w.Header().Add("ETag", "0")
-	WriteURLToWriter(msg, w)
+	_, err := w.Write([]byte(msg))
+	if err != nil {
+		logrus.Error(err)
+	}
+
 }
 
 func WriteObjectToJson(w http.ResponseWriter, object interface{}) (err error) {

dec87a6a54674e13e7b097564c67e081266235d5
Just1689 <Just1689@gmail.com>
2019-02-12 21:38:59 +0200

    Refactor

---
+++ controller/pc.go
@@ -0,0 +1,48 @@
+package controller
+
+import (
+	"cloud.google.com/go/datastore"
+	"context"
+	"fmt"
+	"github.com/plancks-cloud/function-pc/domain"
+	"github.com/plancks-cloud/function-pc/io"
+	"github.com/sirupsen/logrus"
+)
+
+func ListAllServices(db *io.Configuration) (sl []domain.Service, err error) {
+	ctx := context.Background()
+	sl = make([]domain.Service, 0)
+	q := datastore.NewQuery(domain.ServiceCollectionName)
+	_, err = db.DataStoreClient.GetAll(ctx, q, &sl)
+	if err != nil {
+		return nil, fmt.Errorf("DataStore DB: could not list services: %v", err)
+	}
+	return sl, nil
+}
+func StoreServices(db *io.Configuration, id string, sl []domain.Service) (err error) {
+	key := io.GetDataStoreKey(domain.ServiceCollectionName, id)
+	_, err = db.DataStoreClient.Put(context.Background(), key, sl)
+	if err != nil {
+		logrus.Println(err)
+	}
+	return
+}
+
+func ListAllRoutes(db *io.Configuration) (sl []domain.Route, err error) {
+	ctx := context.Background()
+	sl = make([]domain.Route, 0)
+	q := datastore.NewQuery(domain.RouteCollectionName)
+	_, err = db.DataStoreClient.GetAll(ctx, q, &sl)
+	if err != nil {
+		return nil, fmt.Errorf("DataStore DB: could not list routes: %v", err)
+	}
+	return sl, nil
+}
+func StoreRoutes(db *io.Configuration, id string, sl []domain.Route) (err error) {
+	key := io.GetDataStoreKey(domain.RouteCollectionName, id)
+	_, err = db.DataStoreClient.Put(context.Background(), key, sl)
+	if err != nil {
+		logrus.Println(err)
+	}
+	return
+}
--- domain/pc.go
+++ domain/pc.go
@@ -1,56 +1,13 @@
 package domain
 
 import (
-	"cloud.google.com/go/datastore"
-	"context"
-	"fmt"
-	"github.com/plancks-cloud/function-pc/io"
 	"github.com/plancks-cloud/plancks-cloud/model"
-	"github.com/sirupsen/logrus"
 )
 
 const ServiceCollectionName = model.ServiceCollectionName
 
 type Service model.Service
 
-func ListAllServices(db *io.Configuration) (sl []Service, err error) {
-	ctx := context.Background()
-	sl = make([]Service, 0)
-	q := datastore.NewQuery(ServiceCollectionName)
-	_, err = db.DataStoreClient.GetAll(ctx, q, &sl)
-	if err != nil {
-		return nil, fmt.Errorf("DataStore DB: could not list services: %v", err)
-	}
-	return sl, nil
-}
-func StoreServices(db *io.Configuration, id string, sl []Service) (err error) {
-	key := io.GetDataStoreKey(ServiceCollectionName, id)
-	_, err = db.DataStoreClient.Put(context.Background(), key, sl)
-	if err != nil {
-		logrus.Println(err)
-	}
-	return
-}
-
 const RouteCollectionName = model.RouteCollectionName
 
 type Route model.Route
-
-func ListAllRoutes(db *io.Configuration) (sl []Route, err error) {
-	ctx := context.Background()
-	sl = make([]Route, 0)
-	q := datastore.NewQuery(RouteCollectionName)
-	_, err = db.DataStoreClient.GetAll(ctx, q, &sl)
-	if err != nil {
-		return nil, fmt.Errorf("DataStore DB: could not list routes: %v", err)
-	}
-	return sl, nil
-}
-func StoreRoutes(db *io.Configuration, id string, sl []Route) (err error) {
-	key := io.GetDataStoreKey(RouteCollectionName, id)
-	_, err = db.DataStoreClient.Put(context.Background(), key, sl)
-	if err != nil {
-		logrus.Println(err)
-	}
-	return
-}
